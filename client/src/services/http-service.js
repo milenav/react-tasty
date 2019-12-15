62b9f3bcc989f0f7261c49f78daf92d177c7ffcd
MilenaV <milena.n.valcheva@gmail.com>
2019-12-15 22:26:29 +0200

    Extend http service and add doc

--- client/src/services/http-service.js
+++ client/src/services/http-service.js
@@ -1,15 +1,43 @@
 import { getToken } from './auth-service';
 
+/**
+ * Do GET request
+ * @param {String} endpoint 
+ * @param {Object} options 
+ */
 export const get = (endpoint, options = {}) => {
-    return __doRequest('GET', endpoint);
+    return __doRequest('GET', endpoint, null, options);
 };
 
+/**
+ * Do POST request
+ * @param {String} endpoint 
+ * @param {Any} data 
+ * @param {Object} options 
+ */
 export const post = (endpoint, data, options = {}) => {
     return __doRequest('POST', endpoint, data, options);
 };
 
+/**
+ * Do PUT request
+ * @param {String} endpoint 
+ * @param {Any} data 
+ */
+export const put = (endpoint, data, options = {}) => {
+    __doRequest('PUT', endpoint, data, options);
+};
+
+/**
+ * Do delete request
+ * @param {String} endpoint 
+ */
+export const del = (endpoint) => {
+    __doRequest('DELETE', endpoint);
+};
+
 // TODO: Implement error handling for this generic method
-const __doRequest = async (method, endpoint, data = null, opt) => {
+const __doRequest = async (method, endpoint, data = null, opt = {}) => {
     const headers = {'Content-Type': 'Application/json',  'Authorization': `Bearer ${getToken()}`};
     const options = { ...opt, method, headers, };
 
@@ -21,4 +49,4 @@ const __doRequest = async (method, endpoint, data = null, opt) => {
     const json = await response.json();
 
     return json;
-}
+};
